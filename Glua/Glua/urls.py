57e5ceef0e69136a1a53d34a75027e9f72b87681
Henry <henrysalim31@gmail.com>
2025-01-12 19:41:40 +0000

    static files work perfectly

--- Glua/Glua/urls.py
+++ Glua/Glua/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Pharmsave Administration"
 admin.site.site_title = "Pharmsave Admin"
@@ -25,4 +27,4 @@ urlpatterns = [
     path('', include('Inventory.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='Inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Inventory/logout.html'), name='logout')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
