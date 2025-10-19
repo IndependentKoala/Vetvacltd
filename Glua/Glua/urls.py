9ba931898d77eb9ff2e4913d6e3b77b6d9dadb26
Valerie <rene99valerie@gmail.com>
2025-10-19 15:41:10 +0300

    Personalised various pages from Pharmsave to Vetvac

--- Glua/Glua/urls.py
+++ Glua/Glua/urls.py
@@ -19,9 +19,9 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = "Pharmsave Administration"
-admin.site.site_title = "Pharmsave Admin"
-admin.site.index_title = "Welcome to Pharmsave"
+admin.site.site_header = "Vetvac Administration"
+admin.site.site_title = "Vetvac Admin"
+admin.site.index_title = "Welcome to Vetvac"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Inventory.urls')),
