66670724a910c2fdfb75b3ee679673292df1056d
Henry <henrysalim31@gmail.com>
2026-02-10 21:31:24 +0300

    responsive vaccines page and search in the admin section

--- Glua/Inventory/admin.py
+++ Glua/Inventory/admin.py
@@ -3,9 +3,41 @@ from django.core.exceptions import PermissionDenied
 from .models import Drug, Sale, Stocked, Measurement, LockedProduct, MarketingItem, IssuedItem, PickingList, Cannister, IssuedCannister, Client
 
 
+class DrugAdmin(admin.ModelAdmin):
+    list_display = ('name', 'batch_no', 'stock', 'expiry_date', 'dose_pack', 'reorder_level')
+    search_fields = ('name', 'batch_no')
+    list_filter = ('expiry_date', 'stock', 'reorder_level')
+    readonly_fields = ('name', 'batch_no')
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('drug_sold', 'seller', 'client', 'quantity', 'date_sold')
+    search_fields = ('drug_sold', 'client__name', 'seller__username', 'batch_no')
+    list_filter = ('date_sold', 'seller', 'client')
+
+
+class StockedAdmin(admin.ModelAdmin):
+    list_display = ('drug_name', 'number_added', 'supplier', 'staff', 'date_added')
+    search_fields = ('drug_name__name', 'supplier', 'staff__username')
+    list_filter = ('date_added', 'staff', 'drug_name')
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'country_code', 'date_created')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('country_code', 'date_created')
+
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'expiry_date')
+    search_fields = ('name',)
+    list_filter = ('expiry_date',)
+
+
 class LockedProductAdmin(admin.ModelAdmin):
-    # Optionally, add fields to the admin panel
     list_display = ('drug', 'locked_by', 'date_locked', 'quantity', 'client')
+    search_fields = ('drug__name', 'client__name', 'locked_by__username')
+    list_filter = ('date_locked', 'locked_by', 'client')
 
     def save_model(self, request, obj, form, change):
         # Check if the object is being updated (change == True)
@@ -19,12 +51,45 @@ class LockedProductAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-admin.site.register(Drug)  # If you want to use the default admin for Drug
-admin.site.register(Sale)  # If you want to use the default admin for Sale
-admin.site.register(Client)  # Register Client model
-admin.site.register(MarketingItem)  # If you want to use the default admin for marketItem
-admin.site.register(IssuedItem)  # If you want to use the default admin for IssuedItem
+class MarketingItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stock')
+    search_fields = ('name',)
+    list_filter = ('stock',)
+
+
+class IssuedItemAdmin(admin.ModelAdmin):
+    list_display = ('item', 'issued_to', 'quantity_issued', 'issued_by', 'date_issued')
+    search_fields = ('item', 'issued_to', 'issued_by__username')
+    list_filter = ('date_issued', 'issued_by')
+
+
+class PickingListAdmin(admin.ModelAdmin):
+    list_display = ('date', 'client', 'product', 'batch_no', 'quantity')
+    search_fields = ('product', 'batch_no', 'client__name')
+    list_filter = ('date', 'client')
+
+
+class CanisterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'batch_no', 'stock', 'litres')
+    search_fields = ('name', 'batch_no')
+    list_filter = ('stock', 'litres')
+
+
+class IssuedCanisterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'batch_no', 'staff_on_duty', 'returned_by', 'client', 'quantity', 'date_issued')
+    search_fields = ('name', 'batch_no', 'client__name', 'staff_on_duty__username', 'returned_by__username')
+    list_filter = ('date_issued', 'date_returned', 'staff_on_duty', 'returned_by', 'client', 'action')
+
+
+# Register models with their custom admin classes
+admin.site.register(Drug, DrugAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Measurement, MeasurementAdmin)
+admin.site.register(MarketingItem, MarketingItemAdmin)
+admin.site.register(IssuedItem, IssuedItemAdmin)
 admin.site.register(LockedProduct, LockedProductAdmin)
-admin.site.register(PickingList)
-admin.site.register(Cannister)
-admin.site.register(IssuedCannister)
+admin.site.register(PickingList, PickingListAdmin)
+admin.site.register(Cannister, CanisterAdmin)
+admin.site.register(IssuedCannister, IssuedCanisterAdmin)
+admin.site.register(Stocked, StockedAdmin)
--- Glua/users/admin.py
+++ Glua/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(BaseUserAdmin):
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'date_joined')
+
+
+class CustomGroupAdmin(BaseGroupAdmin):
+    search_fields = ('name',)
+    list_filter = ('name',)
+    list_display = ('name',)
+
+
+# Unregister the default User and Group admin
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+# Register with enhanced search
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Group, CustomGroupAdmin)
